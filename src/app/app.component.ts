0c85492c32aeff674cca75f12a7e5559fdd95e0d
John Sourvinos <johnsourvinos@hotmail.com>
2022-05-21 12:52:37 +0300

    Report logo, header and filters

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,10 @@
+import { LogoService } from './logo.service'
 import { Component } from '@angular/core'
-import { Address, Customer } from './customer'
 import { jsPDF } from 'jspdf'
 import '../assets/fonts/NotoSansMonoCondensedRegular.js'
+import '../assets/fonts/PFHandbookProThin.js'
+import '../assets/fonts/ACCanterBold.js'
+import embarkation from '../assets/data/embarkation.json'
 
 @Component({
     selector: 'app-root',
@@ -11,141 +14,137 @@ import '../assets/fonts/NotoSansMonoCondensedRegular.js'
 
 export class AppComponent {
 
-    private topMargin = 30
+    private topMargin = 20
+    private embarkationList = embarkation
+    private pageCount = 1
 
-    private customers: Customer[] = [
-        { 'id': 1, 'description': 'MOBY DICK', 'addresses': [{ 'street': 'Street 1.1' }, { 'street': 'Street 1.2' }] },
-        { 'id': 2, 'description': 'SANDY', 'addresses': [{ 'street': 'Street 2.1' }] },
-        { 'id': 3, 'description': 'BANDOS TRAVEL', 'addresses': [{ 'street': 'Street 3.1' }, { 'street': 'Street 3.2' }] },
-        { 'id': 4, 'description': 'CTS', 'addresses': [] },
-        { 'id': 5, 'description': 'HELLENIC', 'addresses': [{ 'street': 'Street 5.1' }, { 'street': 'Street 5.2' }] },
-        { 'id': 6, 'description': 'TUI', 'addresses': [{ 'street': 'Street 6.1' }] },
-        { 'id': 7, 'description': 'BENITSES TRAVEL', 'addresses': [{ 'street': 'Street 7.1' }, { 'street': 'Street 7.2' }] },
-        { 'id': 8, 'description': 'KAFESAS', 'addresses': [{ 'street': 'Street 8.1' }] },
-        { 'id': 9, 'description': 'AKVILA', 'addresses': [{ 'street': 'Street 9.1' }, { 'street': '9.2' }] },
-        { 'id': 10, 'description': 'HELLENIC', 'addresses': [{ 'street': 'Street 10.1' }, { 'street': '10.2' }] },
-    ]
+    constructor(private logoService: LogoService) { }
 
-    public createJSPDF(): void {
+    public createPDF(): void {
         let nextLineY = this.topMargin
+        const step = 4
         const pdf = new jsPDF('p', 'mm', 'a4')
         pdf.setFont('NotoSansMonoCondensedRegular')
         const pageHeight = parseInt(pdf.internal.pageSize.height.toFixed())
-        console.log(pdf.getFontList())
-        for (let index = 0; index < this.customers.length; index++) {
-            if (nextLineY > pageHeight) {
-                pdf.addPage()
-                nextLineY = this.topMargin
+
+        this.addReportLogo(pdf)
+        this.addReportCriteria(pdf)
+
+        nextLineY += step + 6
+
+        // pdf.text(this.addPageHeader(pdf), 10, nextLineY)
+        nextLineY += step + 2
+
+        pdf.setFont('NotoSansMonoCondensedRegular')
+
+        for (let reservationIndex = 0; reservationIndex < this.embarkationList.embarkation.length; reservationIndex++) {
+            if (this.mustAddPage(nextLineY + this.topMargin, pageHeight)) {
+                // this.addFooter(pdf)
+                nextLineY = this.addPageAndResetTopMargin(pdf)
             }
-            const customer = this.customers[index].id + ' ' + this.customers[index].description + ' ' + nextLineY
-            pdf.setFontSize(20)
-            pdf.setTextColor(0, 0, 0)
-            pdf.text(customer, 10, nextLineY)
-            pdf.setTextColor(227, 60, 47)
-            pdf.setFontSize(10)
-            for (let inner = 0; inner < this.customers[index].addresses.length; inner++) {
-                nextLineY += 10
-                const address = this.customers[index].addresses[0]
-                if (nextLineY > pageHeight) {
-                    pdf.addPage()
-                    nextLineY = this.topMargin
+            pdf.text(this.buildReservationLine(pdf, reservationIndex), 10, nextLineY)
+            for (let passengerIndex = 0; passengerIndex < this.embarkationList.embarkation[reservationIndex].passengers.length; passengerIndex++) {
+                nextLineY += step
+                if (this.mustAddPage(nextLineY + this.topMargin, pageHeight)) {
+                    // this.addFooter(pdf)
+                    nextLineY = this.addPageAndResetTopMargin(pdf)
+                    nextLineY += step + 2
                 }
-                pdf.text(address.street + ' ' + nextLineY, 40, nextLineY)
+                pdf.text(this.buildPassengerLine(pdf, reservationIndex, passengerIndex), 20, nextLineY)
             }
-            nextLineY += 10
+            nextLineY += step
         }
         pdf.output('dataurlnewwindow')
     }
 
-    public createPDF(): void {
-        // 
+    private addPageAndResetTopMargin(pdf: jsPDF): number {
+        pdf.addPage()
+        this.topMargin -= 10
+        this.pageCount++
+        return this.topMargin
     }
 
-    private table(data: Customer[], columnTypes: any[], columns: any[], align: any[]): any {
-        return {
-            table: {
-                headerRows: 1,
-                dontBreakRows: true,
-                body: this.buildTableBody(data, columnTypes, columns, align),
-                heights: 10,
-                bold: false,
-                style: 'table',
-                layout: 'noBorders',
-            },
-            layout: {
-                vLineColor: function (i: number, node: { table: { widths: string | any[] } }): any { return (i === 1 || i === node.table.widths.length - 1) ? '#dddddd' : '#dddddd' },
-                vLineStyle: function (): any { return { dash: { length: 50, space: 0 } } },
-                paddingTop: function (i: number): number { return (i === 0) ? 5 : 5 },
-                paddingBottom: function (): number { return 2 }
-            }
+    private buildReservationLine(pdf: jsPDF, index: number): string {
+        pdf.setFontSize(8)
+        pdf.setTextColor(0, 0, 0)
+        const line =
+            this.embarkationList.embarkation[index].refNo.padEnd(11, ' ') + ' ◽ ' +
+            this.embarkationList.embarkation[index].ticketNo.padEnd(30, ' ') + ' ◽ ' +
+            this.getCustomer(index).padEnd(10, ' ') + ' ◽ ' +
+            this.getDriver(index).padEnd(10, ' ') + ' ◽ ' +
+            this.embarkationList.embarkation[index].ship.padEnd(10, ' ') + ' ◽ ' +
+            this.embarkationList.embarkation[index].totalPersons.toString().padStart(3, ' ') + ' ◽ ' +
+            this.getRemarks(index)
+        return line
+    }
+
+    private buildPassengerLine(pdf: jsPDF, reservationIndex: number, passengeIndex: number): string {
+        pdf.setFontSize(7)
+        pdf.setTextColor(22, 111, 164)
+        const passenger =
+            this.embarkationList.embarkation[reservationIndex].passengers[passengeIndex].lastname.padEnd(30, ' ') + ' ' +
+            this.embarkationList.embarkation[reservationIndex].passengers[passengeIndex].firstname.padEnd(20, ' ') + ' ' +
+            this.embarkationList.embarkation[reservationIndex].passengers[passengeIndex].nationalityDescription.padEnd(30, ' ')
+        return passenger
+    }
+
+    private getCustomer(index: number): string {
+        return this.embarkationList.embarkation[index].customer.substring(0, 10)
+    }
+
+    private getDriver(index: number): string {
+        return this.embarkationList.embarkation[index].driver == undefined ? '(EMPTY)' : this.embarkationList.embarkation[index].driver.substring(0, 10)
+    }
+
+    private getRemarks(index: number): string {
+        return this.embarkationList.embarkation[index].remarks.substring(0, 34)
+    }
+
+    private mustAddPage(nextLineY: number, pageHeight: number): boolean {
+        if (nextLineY > pageHeight) {
+            return true
         }
     }
 
-    private buildTableBody(data: Customer[], columnTypes: any[], columns: any[], align: any[]): void {
-        const body: any = []
-        data.forEach((row) => {
-            let dataRow = []
-            dataRow = this.processRow(columnTypes, columns, row, dataRow, align)
-            body.push(dataRow)
-        })
-        return body
+    private addPageHeader(pdf: jsPDF): string {
+        pdf.setFontSize(8)
+        pdf.setTextColor(82, 32, 36)
+        pdf.setFont('NotoSansMonoCondensedRegular')
+        const pageHeader =
+            'REFNO'.padEnd(11, ' ') + ' ◽ ' +
+            'TICKET'.padEnd(30, ' ') + ' ◽ ' +
+            'CUSTOMER'.padEnd(10, ' ') + ' ◽ ' +
+            'DRIVER'.padEnd(10, ' ') + ' ◽ ' +
+            'SHIP'.padEnd(10, ' ') + ' ◽ ' +
+            'Σ'.padEnd(10, ' ') + ' ◽ ' +
+            'REMARKS'.padEnd(10, ' ')
+        return pageHeader
     }
 
-    private processRow(columnTypes: any[], columns: any[], row: Customer, dataRow: any[], align: any[]): any {
-        columns.forEach((element, index) => {
-            if (element != 'addresses') {
-                dataRow.push({ text: this.formatField(columnTypes[index], row[element]), alignment: align[index].toString(), color: '#000000', noWrap: false })
-                return dataRow
-            } else {
-                dataRow.push(this.buildAddressesTable(row.addresses,
-                    [''],
-                    ['street'],
-                    ['center'])
-                )
-                return dataRow
-            }
-        })
-        // return dataRow
+    private addReportLogo(pdf: jsPDF): void {
+        pdf.setFontSize(20)
+        pdf.setTextColor(173, 0, 0)
+        pdf.addImage(this.logoService.getLogo(), 'JPEG', 10.3, 10, 15, 15)
+        pdf.setFont('ACCanterBold')
+        pdf.text('Corfu Cruises', 30, 18)
+        pdf.setFont('PFHandbookProThin')
+        pdf.setFontSize(10)
+        pdf.setTextColor(0, 0, 0)
+        pdf.text('Embarkation Report', 31.5, 22)
     }
 
-    private formatField(type: any, field: string | number | Date): string {
-        switch (type) {
-            case 'date':
-                return field.toString()
-            default:
-                return field != undefined ? field.toString() : ''
-        }
+    private addReportCriteria(pdf: jsPDF): void {
+        pdf.setFont('PFHandbookProThin')
+        pdf.setTextColor(0, 0, 0)
+        pdf.setFontSize(10)
+        pdf.text('Date ' + '05/06/2022', 202, 12.5, { align: 'right' })
+        pdf.text('Destination ' + 'PAXOS - ANTIPAXOS', 202, 16.5, { align: 'right' })
+        pdf.text('Port ' + 'CORFU PORT', 202, 20.5, { align: 'right' })
+        pdf.text('Ship ' + 'CAPTAIN BILL', 202, 24.5, { align: 'right' })
     }
 
-    private buildAddressesTable(data: Address[], columnTypes: any[], columns: any[], align: any[]): any {
-        return {
-            table: {
-                headerRows: 1,
-                dontBreakRows: true,
-                body: this.buildAddressesBody(data, columnTypes, columns, align),
-                heights: 10,
-                bold: false,
-                style: 'table',
-                layout: 'noBorders',
-            }
-        }
-    }
-
-    private buildAddressesBody(data: Address[], columnTypes: any[], columns: any[], align: any[]): void {
-        const body: any = []
-        data.forEach((row) => {
-            let dataRow = []
-            dataRow = this.processAddressRow(columnTypes, columns, row, dataRow, align)
-            body.push(dataRow)
-        })
-        return body
-    }
-
-    private processAddressRow(columnTypes: any[], columns: any[], row: Address, dataRow: any[], align: any[]): any {
-        columns.forEach((element, index) => {
-            dataRow.push({ text: this.formatField(columnTypes[index], row[element]), alignment: align[index].toString(), color: '#000000', noWrap: false })
-        })
-        return dataRow
+    private addFooter(pdf: jsPDF): void {
+        pdf.text('Pages ' + this.pageCount, 202, 280, { align: 'right' })
     }
 
 }
