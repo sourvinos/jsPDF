73525a31eba126fd165c4dac13dfe4cd854488f4
John Sourvinos <johnsourvinos@hotmail.com>
2022-05-21 19:19:39 +0300

    Report footer and code refactor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,57 +14,102 @@ import embarkation from '../assets/data/embarkation.json'
 
 export class AppComponent {
 
+    // #region variables
+
     private topMargin = 20
+    private lineGap = 4
+    private currentPageCount = 1
+    private nextLineTop = this.topMargin
+    private pageHeight = 0
+    private pdf = new jsPDF()
     private embarkationList = embarkation
-    private pageCount = 1
+
+    //#endregion
 
     constructor(private logoService: LogoService) { }
 
     public createPDF(): void {
-        let nextLineY = this.topMargin
-        const step = 4
-        const pdf = new jsPDF('p', 'mm', 'a4')
-        pdf.setFont('NotoSansMonoCondensedRegular')
-        const pageHeight = parseInt(pdf.internal.pageSize.height.toFixed())
+        this.init()
+        this.addLogo(this.pdf)
+        this.addTitle(this.pdf)
+        this.addCriteria(this.pdf)
+        this.addBody(this.pdf)
+        this.addFooter(this.currentPageCount, this.pdf)
+        this.openPdf()
+    }
 
-        this.addReportLogo(pdf)
-        this.addReportCriteria(pdf)
+    private init(): void {
+        this.pdf = new jsPDF('p', 'mm', 'a4')
+        this.pageHeight = parseInt(this.pdf.internal.pageSize.height.toFixed())
+    }
 
-        nextLineY += step + 6
+    private addLogo(pdf: jsPDF): void {
+        pdf.addImage(this.logoService.getLogo(), 'JPEG', 10.3, 10, 15, 15)
+        pdf.setFont('ACCanterBold')
+        pdf.setFontSize(20)
+        pdf.setTextColor(173, 0, 0)
+        pdf.text('Corfu Cruises', 30, 18)
+    }
 
-        // pdf.text(this.addPageHeader(pdf), 10, nextLineY)
-        nextLineY += step + 2
+    private addTitle(pdf: jsPDF): void {
+        pdf.setFont('PFHandbookProThin')
+        pdf.setFontSize(10)
+        pdf.setTextColor(0, 0, 0)
+        pdf.text('Embarkation Report', 31.5, 22)
+    }
 
-        pdf.setFont('NotoSansMonoCondensedRegular')
+    private addCriteria(pdf: jsPDF): void {
+        pdf.setFont('PFHandbookProThin')
+        pdf.setTextColor(0, 0, 0)
+        pdf.setFontSize(9)
+        pdf.text('Date: ' + '05/06/2022', 202, 12.5, { align: 'right' })
+        pdf.text('Destination: ' + 'PAXOS - ANTIPAXOS', 202, 16.5, { align: 'right' })
+        pdf.text('Port: ' + 'CORFU PORT', 202, 20.5, { align: 'right' })
+        pdf.text('Ship: ' + 'CAPTAIN BILL', 202, 24.5, { align: 'right' })
+    }
 
+    private addBody(pdf: jsPDF): void {
+        this.nextLineTop += this.lineGap + 8
         for (let reservationIndex = 0; reservationIndex < this.embarkationList.embarkation.length; reservationIndex++) {
-            if (this.mustAddPage(nextLineY + this.topMargin, pageHeight)) {
-                // this.addFooter(pdf)
-                nextLineY = this.addPageAndResetTopMargin(pdf)
+            if (this.mustAddPage(this.nextLineTop + this.topMargin, this.pageHeight)) {
+                this.addFooter(this.currentPageCount, pdf)
+                this.currentPageCount++
+                this.nextLineTop = this.addPageAndResetTopMargin(pdf)
             }
-            pdf.text(this.buildReservationLine(pdf, reservationIndex), 10, nextLineY)
+            pdf.text(this.buildReservationLine(pdf, reservationIndex), 10, this.nextLineTop)
             for (let passengerIndex = 0; passengerIndex < this.embarkationList.embarkation[reservationIndex].passengers.length; passengerIndex++) {
-                nextLineY += step
-                if (this.mustAddPage(nextLineY + this.topMargin, pageHeight)) {
-                    // this.addFooter(pdf)
-                    nextLineY = this.addPageAndResetTopMargin(pdf)
-                    nextLineY += step + 2
+                this.nextLineTop += this.lineGap
+                if (this.mustAddPage(this.nextLineTop + this.topMargin, this.pageHeight)) {
+                    this.addFooter(this.currentPageCount, pdf)
+                    this.currentPageCount++
+                    this.nextLineTop = this.addPageAndResetTopMargin(pdf)
+                    this.nextLineTop += this.lineGap + 2
                 }
-                pdf.text(this.buildPassengerLine(pdf, reservationIndex, passengerIndex), 20, nextLineY)
+                pdf.text(this.buildPassengerLine(pdf, reservationIndex, passengerIndex), 20, this.nextLineTop)
             }
-            nextLineY += step
+            this.nextLineTop += this.lineGap
         }
-        pdf.output('dataurlnewwindow')
+    }
+
+    private addFooter(pageCount: number, pdf: jsPDF): void {
+        pdf.setFont('PFHandbookProThin')
+        pdf.setTextColor(0, 0, 0)
+        pdf.setFontSize(9)
+        pdf.text('Page: ' + pageCount.toString(), 202, 290, { align: 'right' })
+    }
+
+    private openPdf(): void {
+        this.pdf.output('dataurlnewwindow')
     }
 
     private addPageAndResetTopMargin(pdf: jsPDF): number {
         pdf.addPage()
         this.topMargin -= 10
-        this.pageCount++
         return this.topMargin
     }
 
     private buildReservationLine(pdf: jsPDF, index: number): string {
+        pdf.setFont('NotoSansMonoCondensedRegular')
         pdf.setFontSize(8)
         pdf.setTextColor(0, 0, 0)
         const line =
@@ -79,6 +124,7 @@ export class AppComponent {
     }
 
     private buildPassengerLine(pdf: jsPDF, reservationIndex: number, passengeIndex: number): string {
+        pdf.setFont('NotoSansMonoCondensedRegular')
         pdf.setFontSize(7)
         pdf.setTextColor(22, 111, 164)
         const passenger =
@@ -100,51 +146,10 @@ export class AppComponent {
         return this.embarkationList.embarkation[index].remarks.substring(0, 34)
     }
 
-    private mustAddPage(nextLineY: number, pageHeight: number): boolean {
-        if (nextLineY > pageHeight) {
+    private mustAddPage(nextLineTop: number, pageHeight: number): boolean {
+        if (nextLineTop > pageHeight) {
             return true
         }
-    }
-
-    private addPageHeader(pdf: jsPDF): string {
-        pdf.setFontSize(8)
-        pdf.setTextColor(82, 32, 36)
-        pdf.setFont('NotoSansMonoCondensedRegular')
-        const pageHeader =
-            'REFNO'.padEnd(11, ' ') + ' ◽ ' +
-            'TICKET'.padEnd(30, ' ') + ' ◽ ' +
-            'CUSTOMER'.padEnd(10, ' ') + ' ◽ ' +
-            'DRIVER'.padEnd(10, ' ') + ' ◽ ' +
-            'SHIP'.padEnd(10, ' ') + ' ◽ ' +
-            'Σ'.padEnd(10, ' ') + ' ◽ ' +
-            'REMARKS'.padEnd(10, ' ')
-        return pageHeader
-    }
-
-    private addReportLogo(pdf: jsPDF): void {
-        pdf.setFontSize(20)
-        pdf.setTextColor(173, 0, 0)
-        pdf.addImage(this.logoService.getLogo(), 'JPEG', 10.3, 10, 15, 15)
-        pdf.setFont('ACCanterBold')
-        pdf.text('Corfu Cruises', 30, 18)
-        pdf.setFont('PFHandbookProThin')
-        pdf.setFontSize(10)
-        pdf.setTextColor(0, 0, 0)
-        pdf.text('Embarkation Report', 31.5, 22)
-    }
-
-    private addReportCriteria(pdf: jsPDF): void {
-        pdf.setFont('PFHandbookProThin')
-        pdf.setTextColor(0, 0, 0)
-        pdf.setFontSize(10)
-        pdf.text('Date ' + '05/06/2022', 202, 12.5, { align: 'right' })
-        pdf.text('Destination ' + 'PAXOS - ANTIPAXOS', 202, 16.5, { align: 'right' })
-        pdf.text('Port ' + 'CORFU PORT', 202, 20.5, { align: 'right' })
-        pdf.text('Ship ' + 'CAPTAIN BILL', 202, 24.5, { align: 'right' })
-    }
-
-    private addFooter(pdf: jsPDF): void {
-        pdf.text('Pages ' + this.pageCount, 202, 280, { align: 'right' })
     }
 
 }
